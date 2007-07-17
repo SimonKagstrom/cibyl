9047fc9c3a4ac583e6669be8c9c24665f19b0a41
Simon Kagstrom <simon.kagstrom@gmail.com>
2007-07-17 19:49:11 +0000

    Added the try/catch pairs

--- include/cibyl.h
+++ include/cibyl.h
@@ -27,14 +27,24 @@ typedef int bool_t;
 /* Special object! */
 typedef int NOPH_Exception_t;
 
-/* Include the generated syscall definitions
+/** Include the generated syscall definitions
  *
  * Thanks to Ian Lance Taylor for the use of .set push / .set pop.
  */
 # include "cibyl-syscall_defs.h"
 
-/* The exception variable is always in register k0 */
+/** The exception variable is always in register k0 */
 register NOPH_Exception_t NOPH_exception asm("$26");
+
+
+extern void __NOPH_try(void (*callback)(NOPH_Exception_t exception));
+#define NOPH_try(callback) __NOPH_try(callback);
+
+extern void __NOPH_catch(void);
+#define NOPH_catch() __NOPH_catch()
+
+
+
 #endif /* __ASSEMBLER__ */
 
 #endif /* !__CIBYL_H__ */
