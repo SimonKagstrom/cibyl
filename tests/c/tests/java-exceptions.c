b7412be6f557585d2a24dddf3e3be1340559c32b
Simon Kagstrom <simon.kagstrom@gmail.com>
2007-07-19 17:20:57 +0000

    Added tests of the try/catch pairs

--- tests/c/tests/java-exceptions.c
+++ tests/c/tests/java-exceptions.c
@@ -12,11 +12,81 @@
 #include <test.h>
 #include <stdlib.h>
 #include <javax/microedition/io.h>
+#include <javax/microedition/lcdui/game.h>
+
+static int exceptions_a;
+int exceptions_b;
+int exceptions_c;
+int caught_exception;
+
+static void handler(NOPH_Exception_t exception)
+{
+  caught_exception = 1;
+  if (exception > 100000)
+    FAIL("Exception number: %x", exception);
+  else
+    PASS("Exception number: %x", exception);
+}
+
+void exception_test_1(void)
+{
+  int i;
+
+  exceptions_a = 1;
+  exceptions_b = 10;
+  caught_exception = 0;
+
+  NOPH_try(handler)
+    {
+      for (i = 10; i >= 0; i--)
+        exceptions_c = exceptions_b / i;
+      exceptions_b = exceptions_a;
+    } NOPH_catch();
+  if (!caught_exception)
+    FAIL("div-by-zero exception: %d", caught_exception);
+  else
+    PASS("div-by-zero excption %d", caught_exception);
+
+  if (exceptions_b == 1)
+    FAIL("executing code after exception: %d != %d", exceptions_b, exceptions_a);
+  else
+    PASS("executing code after exception: %d != %d", exceptions_b, exceptions_a);
+}
+
+void exception_test_2(void)
+{
+  exceptions_a = 1;
+  exceptions_b = 10;
+  caught_exception = 0;
+
+  NOPH_try(handler)
+    {
+      int w = NOPH_GameCanvas_getWidth(-1); /* Should throw an exception */
+      exceptions_b = w;
+    } NOPH_catch();
+  if (!caught_exception)
+    FAIL("array-idx-out exception: %d", caught_exception);
+  else
+    PASS("array-idx-out excption %d", caught_exception);
+
+  if (exceptions_b != 10)
+    FAIL("executing code after exception: %d != %d", exceptions_b, exceptions_a);
+  else
+    PASS("executing code after exception: %d != %d", exceptions_b, exceptions_a);
+}
+
 
 /* The run-the-tests function */
 void exceptions_run(void)
 {
   char *s = "http://spel.bth.se/~ska/index.html";
+
+  exceptions_a = 0;
+  exceptions_b = 0;
+  exceptions_c = 0;
+
+  exception_test_1();
+  exception_test_2();
 
   NOPH_Connector_openDataInputStream(s);
   if (NOPH_exception)
