cde09502438681e25292f0d26344aa3f1bb926a1
Simon Kagstrom <simon.kagstrom@gmail.com>
2007-07-21 10:15:41 +0000

    Handle a few more cases

--- tools/python/Cibyl/PeepholeOptimizer/templates/double_stores.py
+++ tools/python/Cibyl/PeepholeOptimizer/templates/double_stores.py
@@ -165,6 +165,65 @@ class MatchStoreLabelPutstaticLoad(MatchStoreLabelLoad):
 	return items
 
 
+
+# Assuming nothing in the function jumps to LABEL
+#       dup
+#       istore
+# LABEL:
+#       ireturn
+# ->
+# LABEL:
+#       ireturn
+class MatchDupIstoreLabelIreturn(MatchStoreLabelLoad):
+    def __init__(self):
+	Template.__init__(self,
+			  [{"class" : Dup},
+			   {"class" : Istore},
+			   {"class" : Label},
+			   {"class" : Ireturn},
+			   ])
+
+    def match(self, fn, items):
+	# See to it that there are no jumps to LABEL
+	for fnItem in fn.contents:
+	    if isinstance(fnItem, BranchInstruction):
+		if fnItem.target == items[2].name:
+		    return False
+	return True
+
+    # Remove the dup/istore
+    def execute(self, fn, items):
+	items[0] = Nop()
+	items[1] = Nop()
+	return items
+
+
+#	istore $N
+#	iload $N
+# ->
+#       dup
+#       istore $N
+class MatchStoreLoad(Template):
+    def __init__(self):
+	pre = [{"class" : Istore}]
+	post = [{"class" : Iload}]
+	out = pre + post
+
+	Template.__init__(self, out)
+
+    def match(self, fn, items):
+	N = items[0].targetLocal
+	if N != items[1].sourceLocal:
+	    return False
+	return True
+
+    # Remove the istore/iload
+    def execute(self, fn, items):
+        items[1] = items[0]
+	items[0] = Dup()
+	return items
+
+
 #	istore $N
 #	iload  $X, $X != $Y
 #	iload  $N
@@ -204,11 +263,43 @@ class MatchSwap(Template):
 	items[2] = Instruction("\tswap\n")
 	return items
 
+class MatchSwapDup(MatchSwap):
+    def __init__(self):
+	Template.__init__(self,
+			  [{"class" : Istore},
+			   {"class" : Iload},
+			   {"class" : Iload},
+			   {"class" : Instruction},
+			   {"class" : Dup},
+			   {"class" : Istore},
+			   ]
+			  )
+
+    def match(self, fn, items):
+	N = items[0].targetLocal
+	if N == items[1].sourceLocal:
+	    return False
+	if N != items[2].sourceLocal:
+	    return False
+	for item in items[3:-1]:
+	    if not midInstructionValid(N, item):
+		return False
+	return True
+
+    # Remove the istore/iload
+    def execute(self, fn, items):
+	items[0] = Nop()
+	items[2] = Instruction("\tswap\n")
+	return items
+
 for n in range(0, 1):
     for m in range(0, 6):
 	addTemplate(MatchStoreLoadStore(n, m))
 for i in range(0, 6):
     addTemplate(MatchStoreStore(i))
 addTemplate(MatchStoreLabelLoad())
+addTemplate(MatchStoreLoad())
+addTemplate(MatchDupIstoreLabelIreturn())
 addTemplate(MatchStoreLabelPutstaticLoad())
 addTemplate(MatchSwap())
+addTemplate(MatchSwapDup())
