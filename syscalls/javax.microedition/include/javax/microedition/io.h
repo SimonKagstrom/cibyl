3b8e3b33f315f95265aaa8368fee4940b3377d3b
Simon Kagstrom <simon.kagstrom@gmail.com>
2007-08-13 11:57:40 +0000

    Moved stuff to java.io, where it belongs

--- syscalls/javax.microedition/include/javax/microedition/io.h
+++ syscalls/javax.microedition/include/javax/microedition/io.h
@@ -19,11 +19,8 @@ extern "C" {
 #include <stdio.h> /* FILE* */
 
 #include <java/util.h>
+#include <java/io.h>
 
-typedef int NOPH_InputStream_t;
-typedef int NOPH_OutputStream_t;
-typedef int NOPH_DataInputStream_t;
-typedef int NOPH_DataOutputStream_t;
 typedef int NOPH_Connection_t;
 typedef int NOPH_FileConnection_t;
 typedef int NOPH_InputConnection_t;
@@ -41,18 +38,6 @@ NOPH_DataOutputStream_t NOPH_Connector_openDataOutputStream(const char* name); /
 
 void NOPH_FileConnection_close(NOPH_FileConnection_t fc); /* Throws */
 NOPH_Enumeration_t NOPH_FileConnection_list(NOPH_FileConnection_t fc); /* Throws */
-
-/* Input stream stuff */
-int NOPH_InputStream_available(NOPH_InputStream_t is); /* Throws */
-int NOPH_InputStream_read(NOPH_InputStream_t is); /* Throws */
-int NOPH_InputStream_read_into(NOPH_InputStream_t is, char* vec); /* Throws */
-void NOPH_InputStream_close(NOPH_InputStream_t is); /* Throws */
-
-/* Output stream stuff */
-void NOPH_OutputStream_write(NOPH_OutputStream_t os, int b); /* Throws */
-void NOPH_OutputStream_writeInt(NOPH_OutputStream_t os, int i); /* Throws */
-void NOPH_OutputStream_writeShort(NOPH_OutputStream_t os, short i); /* Throws */
-void NOPH_OutputStream_close(NOPH_OutputStream_t os); /* Throws */
 
 /* Record-store stuff */
 NOPH_RecordStore_t NOPH_RecordStore_openRecordStore(char* recordStoreName, bool_t createIfNecessary); /* Throws */
