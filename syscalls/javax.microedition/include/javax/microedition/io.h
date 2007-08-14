f5d85cc30dfddcb0b8e64da66ee3f734d39918ea
Simon Kagstrom <simon.kagstrom@gmail.com>
2007-08-14 13:37:41 +0000

    Added lots of FileConnection stuff and opening of file connections (convenience). Documentation

--- syscalls/javax.microedition/include/javax/microedition/io.h
+++ syscalls/javax.microedition/include/javax/microedition/io.h
@@ -15,11 +15,20 @@
 extern "C" {
 #endif
 
+/**
+ * @file javax/microedition/io.h Functions from the
+ * javax.microedition.io package (plus some helpers)
+ */
+
 #include <cibyl.h>
 #include <stdio.h> /* FILE* */
 
 #include <java/util.h>
 #include <java/io.h>
+
+#define NOPH_Connector_READ       1
+#define NOPH_Connector_WRITE      2
+#define NOPH_Connector_READ_WRITE 3
 
 typedef int NOPH_Connection_t;
 typedef int NOPH_FileConnection_t;
@@ -28,17 +37,28 @@ typedef int NOPH_StreamConnection_t;
 typedef int NOPH_HttpConnection_t;
 typedef int NOPH_RecordStore_t;
 
-/* Network and file stuff */
+/* Connector */
 NOPH_DataInputStream_t NOPH_Connector_openDataInputStream(const char* name); /* Throws */
 NOPH_FileConnection_t NOPH_Connector_openFileConnection(const char* name); /* Not generated */
+NOPH_FileConnection_t NOPH_Connector_openFileConnection_mode(const char* name, int mode); /* Not generated */
 
-NOPH_DataInputStream_t NOPH_FileConnection_openDataInputStream(NOPH_FileConnection_t fc); /* Throws */
+/* FileConnection (JSR-075) */
 NOPH_InputStream_t NOPH_FileConnection_openInputStream(NOPH_FileConnection_t fc); /* Throws */
-
-NOPH_DataOutputStream_t NOPH_Connector_openDataOutputStream(const char* name); /* Throws */
-
+NOPH_OutputStream_t NOPH_FileConnection_openOutputStream(NOPH_FileConnection_t fc, long offset); /* Throws */
+NOPH_DataInputStream_t NOPH_FileConnection_openDataInputStream(NOPH_FileConnection_t fc); /* Throws */
+NOPH_DataOutputStream_t NOPH_FileConnection_openDataOutputStream(NOPH_FileConnection_t fc); /* Throws */
+bool_t NOPH_FileConnection_exists(NOPH_FileConnection_t fc); /* Throws */
+bool_t NOPH_FileConnection_canWrite(NOPH_FileConnection_t fc); /* Throws */
+bool_t NOPH_FileConnection_canRead(NOPH_FileConnection_t fc); /* Throws */
+bool_t NOPH_FileConnection_isOpen(NOPH_FileConnection_t fc); /* Throws */
+void NOPH_FileConnection_truncate(NOPH_FileConnection_t fc, long byteOffest); /* Throws */
+long NOPH_FileConnection_fileSize(NOPH_FileConnection_t fc); /* Throws */
+void NOPH_FileConnection_create(NOPH_FileConnection_t fc); /* Throws */
 void NOPH_FileConnection_close(NOPH_FileConnection_t fc); /* Throws */
 NOPH_Enumeration_t NOPH_FileConnection_list(NOPH_FileConnection_t fc); /* Throws */
+
+
+NOPH_DataOutputStream_t NOPH_Connector_openDataOutputStream(const char* name); /* Throws */
 
 /* Record-store stuff */
 NOPH_RecordStore_t NOPH_RecordStore_openRecordStore(char* recordStoreName, bool_t createIfNecessary); /* Throws */
@@ -51,6 +71,20 @@ int NOPH_RecordStore_getRecordSize(NOPH_RecordStore_t rs, int recordId); /* Thro
 int NOPH_RecordStore_addRecord(NOPH_RecordStore_t rs, char* newData, int offset, int numBytes); /* Not generated */
 void NOPH_RecordStore_setRecord(NOPH_RecordStore_t rs, int recordId, char* newData, int offset, int numBytes); /* Not generated */
 int NOPH_RecordStore_getRecord(NOPH_RecordStore_t rs, int recordId, char* buffer, int offset); /* Not generated */
+
+/* -- Non-java functionality -- */
+/**
+ * Create a ANSI C file from a FileConnection. This is a convenience
+ * method to use instead of creating a file connection, opening a
+ * stream from it and convert it to a FILE ptr.
+ *
+ * @param name the name of the file to open
+ * @param mode the mode to open the file in
+ *
+ * @return a pointer to the new FILE input stream
+ */
+extern FILE *NOPH_FileConnection_openFILE(const char *name, const char *mode);
+
 
 #if defined(__cplusplus)
 }
