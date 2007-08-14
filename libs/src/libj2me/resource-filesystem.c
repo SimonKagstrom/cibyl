41759b2cf77fc94e7db25ff5d208215016dc26a4
Simon Kagstrom <simon.kagstrom@gmail.com>
2007-08-14 13:36:54 +0000

    Bug fixes: Open via a class, initialize error

--- libs/src/libj2me/resource-filesystem.c
+++ libs/src/libj2me/resource-filesystem.c
@@ -24,17 +24,18 @@ static FILE *open(const char *path,
                   cibyl_fops_open_mode_t mode)
 {
   FILE *fp;
-  NOPH_GameCanvas_t gc = NOPH_GameCanvas_get();
   NOPH_InputStream_t is;
   resource_file_t *p;
-  int error;
+  int error = 0;
 
   /* Try to open the resource stream. They are always read-only, so
    * just ignore the mode
    */
   NOPH_try(exception_handler, (void*)&error)
     {
-      is = NOPH_Class_getResourceAsStream(gc, (char*)path);
+      NOPH_GameCanvas_t gc = NOPH_GameCanvas_get();
+      NOPH_Class_t cl = NOPH_Object_getClass(gc);
+      is = NOPH_Class_getResourceAsStream(cl, (char*)path);
     } NOPH_catch();
   if (error)
     return NULL;
@@ -73,5 +74,5 @@ static void __attribute__((constructor))register_fs(void)
   resource_fops.eof   = NOPH_InputStream_fops.eof;
   resource_fops.flush = NOPH_InputStream_fops.flush;
 
-  cibyl_register_fops(&resource_fops, 1);
+  cibyl_fops_register(&resource_fops, 1);
 }
