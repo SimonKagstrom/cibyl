12016561203c2c42a9d50debeb3ab1e77f3dcdaf
Simon Kagstrom <simon.kagstrom@gmail.com>
2007-08-14 15:49:01 +0000

    Moved fc stuff to jsr075 library

--- libs/src/libj2me/connector-filesystem.c
+++ libs/src/libj2me/connector-filesystem.c
@@ -17,87 +17,12 @@ typedef struct
   char *path;
 } connector_file_t;
 
-static void exception_handler(NOPH_Exception_t ex, void *arg)
-{
-  int *p = (int*)arg;
-  *p = 1;
-
-  NOPH_delete(ex);
-}
-
-FILE *NOPH_FileConnection_openFILE(const char *path, const char *in_mode)
-{
-  cibyl_fops_open_mode_t mode = cibyl_file_get_mode(in_mode);
-  NOPH_FileConnection_t fc;
-  FILE *fp;
-  connector_file_t *p;
-  int error = 0;
-
-  /* Handle the write case by simply returning a FILE for the output stream */
-  if (mode == WRITE || mode == APPEND || mode == READ_TRUNCATE)
-    {
-      NOPH_OutputStream_t os;
-      FILE *out;
-
-      fc = NOPH_Connector_openFileConnection_mode(path, NOPH_Connector_WRITE);
-      os = NOPH_FileConnection_openDataOutputStream(fc); /* Can throw stuff */
-
-      /* Create and maybe truncate the file */
-      NOPH_try(exception_handler, (void*)&error)
-        {
-          if (!NOPH_FileConnection_exists(fc))
-            NOPH_FileConnection_create(fc);
-          if (mode == READ_TRUNCATE)
-            NOPH_FileConnection_truncate(fc, 0);
-        } NOPH_catch();
-      if (error)
-        {
-          NOPH_OutputStream_close(os);
-          return NULL;
-        }
-      out = NOPH_OutputStream_createFILE(os);
-
-      if (!out)
-        {
-          NOPH_OutputStream_close(os);
-          return NULL;
-        }
-
-      return out;
-    }
-
-  fp = cibyl_file_alloc(&connector_fops);
-  p = (connector_file_t *)fp->priv;
-
-  if (mode != READ)
-    NOPH_throw(NOPH_Exception_new_string("Opening a connector stream with invalid mode"));
-  /* Try to open the connector stream. */
-  NOPH_try(exception_handler, (void*)&error)
-    {
-      fc = NOPH_Connector_openFileConnection_mode(p->path, NOPH_Connector_READ);
-      p->is_file.is = NOPH_FileConnection_openDataInputStream(fc);
-    } NOPH_catch();
-  if (error)
-    {
-      cibyl_file_free(fp);
-      return NULL;
-    }
-
-  p->path = strdup(path);
-  p->is_file.eof = 0;
-  p->is_file.is_fp = 0;
-
-  return fp;
-}
-
 static FILE *open(const char *path,
                   cibyl_fops_open_mode_t mode)
 {
   FILE *fp;
   connector_file_t *p;
   int error = 0;
-
-  printf("Conn: trying to open %s:%d!\n", path, mode);
 
   /* Handle the write case by simply returning a FILE for the output stream */
   if (mode == WRITE || mode == APPEND)
@@ -120,7 +45,7 @@ static FILE *open(const char *path,
   if (mode != READ)
     NOPH_throw(NOPH_Exception_new_string("Opening a connector stream with invalid mode"));
   /* Try to open the connector stream. */
-  NOPH_try(exception_handler, (void*)&error)
+  NOPH_try(NOPH_setter_exception_handler, (void*)&error)
     {
       p->is_file.is = NOPH_Connector_openDataInputStream((char*)path);
     } NOPH_catch();
@@ -148,75 +73,18 @@ static int close(FILE *fp)
   return 0;
 }
 
-static void reset(FILE *fp)
-{
-  connector_file_t *p = (connector_file_t *)fp->priv;
-  int error = 0;
-
-  NOPH_try(exception_handler, (void*)&error)
-    {
-      NOPH_InputStream_reset(p->is_file.is);
-    } NOPH_catch();
-  if (error)
-    {
-      NOPH_FileConnection_t fc;
-
-      /* FileConnections - reset by reopening */
-      NOPH_InputStream_close(p->is_file.is);
-      fc = NOPH_Connector_openFileConnection(p->path);
-      p->is_file.is = NOPH_FileConnection_openInputStream(fc);
-    }
-}
-
-static int seek(FILE *fp, long offset, int whence)
-{
-  connector_file_t *p = (connector_file_t *)fp->priv;
-  int skip = offset;
-  int avail;
-  int error = 0;
-
-  /* Only inputstreams can support seeking */
-  switch (whence)
-    {
-    case SEEK_SET:
-      p->is_file.is_fp = 0;
-      reset(fp);
-      break;
-    case SEEK_END:
-      p->is_file.is_fp = 0;
-      avail = NOPH_InputStream_available(p->is_file.is);
-      skip = avail - offset;
-      reset(fp);
-      break;
-    case SEEK_CUR:
-      /* Do nothing (fallthrough) */
-    default:
-      break;
-    }
-  NOPH_try(exception_handler, (void*)&error)
-    {
-      p->is_file.is_fp += NOPH_InputStream_skip(p->is_file.is, skip);
-    } NOPH_catch();
-  if (error)
-    return -1;
-
-  return 0;
-}
-
-
 /* The fops structure for connector files */
 static cibyl_fops_t connector_fops =
 {
-  .uri = "file://",
   .keep_uri = 1,
   .priv_data_size = sizeof(connector_file_t),
   .open  = open,
   .close = close,
   .read  = NULL, /* Set below */
-  .write = NULL, /* Set below */
-  .seek  = seek,
-  .tell  = NULL, /* Set below */
-  .eof   = NULL, /* Set below */
+  .write = NULL, /* ... */
+  .seek  = NULL,
+  .tell  = NULL,
+  .eof   = NULL,
 };
 
 static void __attribute__((constructor))register_fs(void)
@@ -224,8 +92,16 @@ static void __attribute__((constructor))register_fs(void)
   /* By default uses the same implementations as input streams */
   connector_fops.read  = NOPH_InputStream_fops.read;
   connector_fops.write = NOPH_InputStream_fops.write;
+  connector_fops.seek  = NOPH_InputStream_fops.seek;
   connector_fops.tell  = NOPH_InputStream_fops.tell;
   connector_fops.eof   = NOPH_InputStream_fops.eof;
 
-  cibyl_fops_register(&connector_fops, 0);
+  /* Lots of different types are handle by the connector */
+  cibyl_fops_register("file://", &connector_fops, 0); /* Can be overridden by jsr075 */
+  cibyl_fops_register("http://", &connector_fops, 0);
+  cibyl_fops_register("https://", &connector_fops, 0);
+  cibyl_fops_register("datagram://", &connector_fops, 0);
+  cibyl_fops_register("comm:", &connector_fops, 0);
+  cibyl_fops_register("socket://", &connector_fops, 0);
+  cibyl_fops_register("ssl://", &connector_fops, 0);
 }
