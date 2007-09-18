69653799263b6943a8d1a926e5d245b509bd03ae
Simon Kagstrom <simon.kagstrom@gmail.com>
2007-09-18 04:55:19 +0000

    Added lb/lh memory tests, better lwl/swl tests, simpler structure

--- tests/c/tests/memory.c
+++ tests/c/tests/memory.c
@@ -12,10 +12,47 @@
 #include <stdint.h>
 #include <test.h>
 
-uint8_t vals[] =
+uint8_t memory_vals[] =
 {
-  0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a
+  /* 0 */ 0xff, 0xfe, 0xfd, 0xff,
+  /* 4 */ 0x1f, 0x1e, 0x1d, 0xff,
+  /* 8 */ 0xff, 0xff
 };
+
+
+void memory_read_byte_unsigned(uint8_t *p, uint8_t result)
+{
+  if (*p != result)
+    FAIL("lbu %x != %x", *p, result);
+  else
+    PASS("lbu %x == %x", *p, result);
+}
+
+void memory_read_byte(int8_t *p, int8_t result)
+{
+  if (*p != result)
+    FAIL("lb %x != %x", *p, result);
+  else
+    PASS("lb %x == %x", *p, result);
+}
+
+
+void memory_read_short_unsigned(uint16_t *p, uint16_t result)
+{
+  if (*p != result)
+    FAIL("lhu %x != %x", *p, result);
+  else
+    PASS("lhu %x == %x", *p, result);
+}
+
+void memory_read_short(int16_t *p, int16_t result)
+{
+  if (*p != result)
+    FAIL("lh %x != %x", *p, result);
+  else
+    PASS("lh %x == %x", *p, result);
+}
+
 
 struct tjoho
 {
@@ -24,7 +61,8 @@ struct tjoho
 } __attribute__((packed));
 
 struct tjoho misalign = {0x1, 0x02030405};
-struct tjoho misalign2 = {0x1, 1234};
+struct tjoho misalign2 = {0x1, 0xff001234};
+struct tjoho misalign3 = {0xff, 0x02030405};
 
 uint8_t dst[10];
 
@@ -38,32 +76,55 @@ void memory_swl(struct tjoho *dst, int val)
   dst->b = val;
 }
 
+void memory_test_lwl(struct tjoho *src, int correct)
+{
+  int val = memory_lwl(src);
+
+  if (val != correct)
+    FAIL("lwl: 0x%x != 0x%x\n", val, correct);
+  else
+    PASS("lwl: 0x%x != 0x%x\n", val, correct);
+}
+
+void memory_test_swl(struct tjoho *src, int val,
+                     uint8_t a, uint8_t b, uint8_t c, uint8_t d)
+{
+  uint8_t *p = (char*)&src->b;
+
+  memory_swl(src, val);
+
+  if (p[0] != a || p[1] != b || p[2] != c || p[3] != d)
+    FAIL("swl: 0x%x != 0x%02x%02x%02x%02x\n", val, p[0], p[1], p[2], p[3]);
+  else
+    PASS("swl: 0x%x != 0x%02x%02x%02x%02x\n", val, p[0], p[1], p[2], p[3]);
+}
+
 /* The run-the-tests function */
 void memory_run(void)
 {
   struct tjoho tmp;
-  char *a;
-  int v;
 
-  v = memory_lwl(&misalign);
-  if ( v != 0x02030405 )
-    FAIL("lwl: 0x%x != 0x%x\n", v, 0x02030405 );
-  else
-    PASS("lwl: 0x%x\n", v);
+  memory_read_byte_unsigned(&memory_vals[0], (uint8_t)0xff);
+  memory_read_byte_unsigned(&memory_vals[1], (uint8_t)0xfe);
+  memory_read_byte_unsigned(&memory_vals[4], (uint8_t)0x1f);
+  memory_read_byte_unsigned(&memory_vals[5], (uint8_t)0x1e);
 
-  v = memory_lwl(&misalign2);
-  if ( v != 1234 )
-    FAIL("lwl: 0x%x != 0x%x\n", v, 1234 );
-  else
-    PASS("lwl: 0x%x\n", v);
+  memory_read_byte((int8_t*)&memory_vals[0], (int8_t)0xff);
+  memory_read_byte((int8_t*)&memory_vals[1], (int8_t)0xfe);
+  memory_read_byte((int8_t*)&memory_vals[4], (int8_t)0x1f);
+  memory_read_byte((int8_t*)&memory_vals[5], (int8_t)0x1e);
 
-  memory_swl(&tmp, 0x02030405);
-  a = (char*)&tmp.b;
-  if (a[0] != 0x02 ||
-      a[1] != 0x03 ||
-      a[2] != 0x04 ||
-      a[3] != 0x05)
-    FAIL("swl: 0x%x != 0x%x\n", tmp.b, 0x02030405);
-  else
-    PASS("swl: 0x%x\n", tmp.b);
+  memory_read_short_unsigned((uint16_t*)(&memory_vals[0]), (uint16_t)0xfffe);
+  memory_read_short_unsigned((uint16_t*)(&memory_vals[6]), (uint16_t)0x1dff);
+
+  memory_read_short((int16_t*)&memory_vals[0], (int16_t)0xfffe);
+  memory_read_short((int16_t*)&memory_vals[6], (int16_t)0x1dff);
+
+  /* Test lwl and swl */
+  memory_test_lwl(&misalign, 0x02030405);
+  memory_test_lwl(&misalign2, 0xff001234);
+  memory_test_lwl(&misalign3, 0x02030405);
+
+  tmp = misalign2;
+  memory_test_swl(&tmp, 0xff001234, 0xff, 0x00, 0x12, 0x34);
 }
