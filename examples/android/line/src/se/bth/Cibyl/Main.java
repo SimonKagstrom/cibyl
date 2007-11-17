64dc76a67d5d53ec92f3a55067a2315501f70da0
Simon Kagstrom <simon.kagstrom@gmail.com>
2007-11-17 09:01:51 +0000

    Added (empty) example

---
+++ examples/android/line/src/se/bth/Cibyl/Main.java
@@ -0,0 +1,15 @@
+package se.bth.Cibyl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity
+{
+    /** Called with the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle)
+    {
+        super.onCreate(icicle);
+        setContentView(R.layout.main);
+    }
+}
