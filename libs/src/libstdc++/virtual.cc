7fdbd3b91ce384a92f5417414fdbe2569473eaa8
Simon Kagstrom <simon.kagstrom@gmail.com>
2007-12-30 15:27:46 +0000

    Added some more c++ stuff

--- libs/src/libstdc++/virtual.cc
+++ libs/src/libstdc++/virtual.cc
@@ -18,3 +18,30 @@ extern "C" void __cxa_pure_virtual()
   printf("Pure virtual function called\n");
   exit(1);
 }
+
+extern "C" void __cxa_atexit(void (*arg1)(void*), void* arg2, void* arg3)
+{
+}
+
+extern "C" {
+void*  __dso_handle = (void*) &__dso_handle;
+}
+
+typedef int __guard __attribute__((mode (__DI__)));
+
+extern "C"
+int __cxa_guard_acquire (__guard *g)
+{
+    return !*(char *)(g);
+}
+
+extern "C"
+void __cxa_guard_release (__guard *g)
+{
+    *(char *)g = 1;
+}
+
+extern "C"
+void __cxa_guard_abort (__guard *)
+{
+}
