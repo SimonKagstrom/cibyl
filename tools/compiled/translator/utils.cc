161b513e75975fc060761dadccc09da4f9671cfb
Simon Kagstrom <simon.kagstrom@gmail.com>
2008-03-09 18:13:16 +0000

    Removed printout

--- tools/compiled/translator/utils.cc
+++ tools/compiled/translator/utils.cc
@@ -88,7 +88,6 @@ void *read_cpp(size_t *out_size, const char **defines, const char *fmt, ...)
       fprintf(stderr, "popen %s failed\n", path);
       exit(1);
     }
-  printf("Size of %s: %d\n", path + l, buf.st_size);
   data = xcalloc(buf.st_size * 4, 1);
 
   while ( (size = fread(data, 1, buf.st_size * 4, f)) != 0)
