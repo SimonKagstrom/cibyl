f975a17aa5c5fb5901786e28caa3f5d188ceee41
Simon Kagstrom <simon.kagstrom@gmail.com>
2008-03-09 17:44:52 +0000

    Some build fixes to work with the new build way

--- build/Rules-java.mk
+++ build/Rules-java.mk
@@ -9,15 +9,16 @@
 ## $Id: Rules-java.mk 14155 2007-03-11 09:43:00Z ska $
 ##
 ######################################################################
-SOURCES   ?= src/StandaloneMain.java src/CRunTime.java src/Syscalls.java src/CibylConfig.java
+SOURCES   ?= tmpclasses/StandaloneMain.java tmpclasses/CRunTime.java \
+             tmpclasses/Syscalls.java tmpclasses/CibylConfig.java
 GEN_SYSCALL_OPTS ?=
 CIBYL_JAVA_OPTS =
-EXTRA_CLEAN=tmpclasses/ classes/ res/ src/ $(SOURCES) .dirs .jar_built *~ c/syscalls.h
+EXTRA_CLEAN=tmpclasses/ classes/ res/ $(SOURCES) .dirs .jar_built *~ c/syscalls.h
 
 CIBYL_SYSCALL_DIR    ?= $(CIBYL_BASE)/syscalls
 CIBYL_SYSCALL_SETS   ?= ansi
 CIBYL_GENERATE_JAVA_WRAPPERS_OPTS += -D NOJ2ME
-CIBYL_MIPS2JAVA_OPTS ?=
+CIBYL_MIPS2JAVA_OPTS ?= -DNOJ2ME
 
 all: $(SUBDIRS) .dirs tmpclasses/Cibyl.class res/program.data.bin $(RESOURCES) classes/.rebuilt $(TARGET)
 
