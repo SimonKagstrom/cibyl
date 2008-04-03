c5681a9574c56a2b6f3e237ec510ee076cf33e06
Simon Kagstrom <simon.kagstrom@gmail.com>
2008-04-03 17:51:40 +0000

    Added Susmith M R's example

--- examples/cpp-test/c/main.cc
+++ examples/cpp-test/c/main.cc
@@ -21,10 +21,43 @@ int Tjoho::getValue()
   return this->value;
 }
 
+class Base
+{
+public:
+  virtual void function()
+  {
+    printf("\n In Base Function");
+  }
+  virtual void function1()
+  {
+    printf("\n In Base Function1");
+  }
+};
+
+class Derived:public Base
+{
+public:
+  void function()
+  {
+    printf("\n In Derived function");
+  }
+  void function1()
+  {
+    printf("\n In Derived function1");
+  }
+};
+
+
 int main(int argc, char **argv)
 {
   Tjoho t(9);
   Tjoho *p = new Tjoho(15);
+  Base *B;
+  Derived D;
+
+  B = &D;
+  B->function();
+  B->function1();
 
   printf("s: %d, d: %d\n", t.getValue(), p->getValue());
   return 0;
