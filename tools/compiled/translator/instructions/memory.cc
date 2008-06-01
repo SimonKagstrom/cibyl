a45ca6040d908ec544bca3f7b2c88ea6ef921c59
Simon Kagstrom <simon.kagstrom@gmail.com>
2008-06-01 20:12:09 +0000

    Provide "inlined" implementations of lb/lh/lhu etc when there is space

--- tools/compiled/translator/instructions/memory.cc
+++ tools/compiled/translator/instructions/memory.cc
@@ -123,10 +123,10 @@ private:
   const char *convert;
 };
 
-class LW : public LoadXX
+class Lw : public LoadXX
 {
 public:
-  LW(uint32_t address, int opcode,
+  Lw(uint32_t address, int opcode,
      MIPS_register_t rs, MIPS_register_t rt, int32_t extra) : LoadXX("", address, opcode, rs, rt, extra)
   {
   }
@@ -152,10 +152,142 @@ public:
   };
 };
 
-class SW : public StoreXX
+class PartialLoad : public LoadXX
+{
+protected:
+  PartialLoad(int word_size, bool is_signed,  const char *what, uint32_t address, int opcode,
+              MIPS_register_t rs, MIPS_register_t rt, int32_t extra) : LoadXX(what, address, opcode, rs, rt, extra)
+  {
+    this->word_size = word_size;
+    this->is_signed = is_signed;
+
+    panic_if(this->word_size != 8 && this->word_size != 16,
+             "Emitting load of invalid length %d\n", this->word_size);
+  }
+
+  bool pass2()
+  {
+    JavaMethod *mt = controller->getMethodByAddress(this->getAddress());
+
+    panic_if(!mt,
+             "No method for instruction at 0x%x\n",
+             this->getAddress());
+
+    if ( config->optimizePartialMemoryOps ||
+         !config->optimizeInlines)
+      return LoadXX::pass2();
+    if (mt->getBytecodeSize() > 32768)
+      {
+        /* Bytecode size too large to allow for direct inlining,
+         * reverting to normal */
+        emit->warning("Bytecode size in %s is too large for direct inlining of lb/lh",
+                      mt->getName());
+        return LoadXX::pass2();
+      }
+
+    int b_v = 3; /* Assume lb(u) */
+    unsigned int mask_val = 0xff;
+
+    if (this->word_size == 16)
+      {
+        b_v = 2;
+        mask_val = 0xffff;
+      }
+    /* Maybe skip ra */
+    if (this->rt == R_RA)
+      return true;
+    if (this->prefix)
+      this->prefix->pass2();
+
+    emit->bc_pushregister(R_MEM);
+    emit->bc_pushindex(this->rs, this->extra);
+    emit->bc_iaload();
+
+    /* b = 3 - (address & 3) */
+    emit->bc_pushconst(b_v);
+    emit->bc_pushregister(this->rs);
+    if (this->extra != 0)
+      {
+        emit->bc_pushconst(extra);
+        emit->bc_iadd();
+      }
+    emit->bc_pushconst(b_v);
+    emit->bc_iand();
+    emit->bc_isub();
+
+    /* b = b * 8 */
+    emit->bc_pushconst(3);
+    emit->bc_ishl();
+
+    /* out = (val >>> b) & 0xff */
+    emit->bc_iushr();
+    if (this->word_size == 8 && this->is_signed) /* lb */
+      emit->bc_i2b();
+    else if (this->word_size == 16 && !this->is_signed) /* lhu */
+      emit->bc_i2c();
+    else if (this->word_size == 16 && this->is_signed) /* lh */
+      emit->bc_i2s();
+    else /* lbu */
+      {
+        emit->bc_pushconst(mask_val);
+        emit->bc_iand();
+      }
+    emit->bc_popregister( this->rt );
+
+    return true;
+  }
+
+  virtual size_t bytecodeSize(void)
+  {
+    return 24;
+  };
+protected:
+  int word_size;
+  bool is_signed;
+};
+
+
+class Lbu : public PartialLoad
 {
 public:
-  SW(uint32_t address, int opcode,
+  Lbu(uint32_t address, int opcode,
+      MIPS_register_t rs, MIPS_register_t rt, int32_t extra) : PartialLoad(8, false, "ByteUnsigned", address, opcode, rs, rt, extra)
+  {
+  }
+};
+
+class Lb : public PartialLoad
+{
+public:
+  Lb(uint32_t address, int opcode,
+     MIPS_register_t rs, MIPS_register_t rt, int32_t extra) : PartialLoad(8, true, "Short", address, opcode, rs, rt, extra)
+  {
+  }
+};
+
+class Lhu : public PartialLoad
+{
+public:
+  Lhu(uint32_t address, int opcode,
+      MIPS_register_t rs, MIPS_register_t rt, int32_t extra) : PartialLoad(16, false, "ShortUnsigned", address, opcode, rs, rt, extra)
+  {
+  }
+};
+
+class Lh : public PartialLoad
+{
+public:
+  Lh(uint32_t address, int opcode,
+     MIPS_register_t rs, MIPS_register_t rt, int32_t extra) : PartialLoad(16, true, "Short", address, opcode, rs, rt, extra)
+  {
+  }
+};
+
+
+class Sw : public StoreXX
+{
+public:
+  Sw(uint32_t address, int opcode,
      MIPS_register_t rs, MIPS_register_t rt, int32_t extra) : StoreXX("Word", address, opcode, rs, rt, extra)
   {
   }
