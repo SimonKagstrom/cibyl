SimonKagstrom/cibyl

commit 167ea80eff96fee40475e6cdc780937d0bfb8c5f

translator: Prune arguments from DWARF info
Arguments which aren't used (by the higher-level code) doesn't need to
be passed via the A-registers. This commit therefore prunes the ones
which aren't used.

tools/compiled/translator/javamethod.cc
@@ -71,6 +71,16 @@ JavaMethod::JavaMethod(Function **fns,
   this->registerIndirectJumps = false;
   this->m_returnSize = -1;
 
+  this->m_possibleArguments = (MIPS_register_t*)xcalloc(7,
+      sizeof(MIPS_register_t));
+  this->m_possibleArguments[0] = R_SP;
+  this->m_possibleArguments[1] = R_FNA;
+  this->m_possibleArguments[2] = R_A0;
+  this->m_possibleArguments[3] = R_A1;
+  this->m_possibleArguments[4] = R_A2;
+  this->m_possibleArguments[5] = R_A3;
+  this->m_possibleArguments[6] = R_ZERO;
+
   this->address = 0;
   this->size = 0;
   for (int i = 0; i < this->n_functions; i++)
@@ -163,6 +173,21 @@ bool JavaMethod::pass1()
   if (this->hasMultipleFunctions())
     this->registerUsage[R_FNA]++;
 
+  ElfSymbol *sym = elf->getSymbolByAddr(this->getAddress());
+  /* First see which arguments need to be passed */
+  if (config->optimizeFunctionReturnArguments && sym)
+    {
+      /* Zero-terminate at the last needed reg */
+      if (sym->n_args <= 3)
+        this->m_possibleArguments[5] = R_ZERO;
+      if (sym->n_args <= 2)
+        this->m_possibleArguments[4] = R_ZERO;
+      if (sym->n_args <= 1)
+        this->m_possibleArguments[3] = R_ZERO;
+      if (sym->n_args == 0)
+        this->m_possibleArguments[2] = R_ZERO;
+    }
+
   for (MIPS_register_t reg = this->getFirstRegisterToPass(&it);
       reg != R_ZERO;
       reg = this->getNextRegisterToPass(&it))
@@ -181,7 +206,6 @@ bool JavaMethod::pass1()
     emit->warning("Function clobbers V1 but not V0");
 
   /* Update the size from the ELF, if it's known */
-  ElfSymbol *sym = elf->getSymbolByAddr(this->getAddress());
   if (config->optimizeFunctionReturnArguments && sym)
     {
       if (sym->ret_size >= 0 && sym->ret_size <= 2)
@@ -366,7 +390,7 @@ bool JavaMethod::pass2()
                this->getName(), this->registerUsage[i]);
 
       /* These are passed as arguments, skip them */
-      if (i == R_FNA || i == R_SP || i == R_A0 || i == R_A1 || i == R_A2 || i == R_A3)
+      if (this->registerIsArgument((MIPS_register_t)i))
         continue;
 
       /* If this register is used, initialize it */
@@ -494,9 +518,6 @@ bool JavaMethod::pass2()
   return out;
 }
 
-/* R_ZERO terminates it */
-static MIPS_register_t possibleArgumentRegs[] = { R_SP, R_A0, R_A1, R_A2, R_A3, R_FNA, R_ZERO };
-
 MIPS_register_t JavaMethod::getFirstRegisterToPass(void *_it)
 {
   int *it = (int*)_it;
@@ -510,12 +531,25 @@ MIPS_register_t JavaMethod::getNextRegisterToPass(void *_it)
   int *it = (int*)_it;
   MIPS_register_t ret;
 
-  while (possibleArgumentRegs[*it] != R_ZERO &&
-	 this->clobbersReg( possibleArgumentRegs[*it] ) == 0)
+  while (this->m_possibleArguments[*it] != R_ZERO &&
+	 this->clobbersReg(this->m_possibleArguments[*it] ) == 0)
       (*it)++;
-  ret = possibleArgumentRegs[*it];
+  ret = this->m_possibleArguments[*it];
   (*it)++;
   return ret;
+}
+
+bool JavaMethod::registerIsArgument(MIPS_register_t reg)
+{
+  int i;
+
+  for (i = 0; this->m_possibleArguments[i] != R_ZERO; i++)
+    {
+      if (this->m_possibleArguments[i] == reg)
+        return true;
+    }
+
+  return false;
 }
 
 int JavaMethod::returnSize()

tools/compiled/translator/calltablemethod.cc
@@ -114,23 +114,29 @@ void CallTableMethod::generateMethod(const char *name,
       emit->generic("%s.%s(", cl->getName(), mt->getName());
 
       /* Pass registers */
-      if (mt->clobbersReg( R_SP ))
-        { emit->generic("sp"); comma = ","; }
-      if (mt->clobbersReg( R_A0 ))
-        { emit->generic("%s a0", comma); comma = ","; }
-      if (mt->clobbersReg( R_A1 ))
-        { emit->generic("%s a1", comma); comma = ","; }
-      if (mt->clobbersReg( R_A2 ))
-        { emit->generic("%s a2", comma); comma = ","; }
-      if (mt->clobbersReg( R_A3 ))
-        { emit->generic("%s a3", comma); comma = ","; }
-      if (mt->clobbersReg( R_FNA ))
+      void *it;
+      for (MIPS_register_t reg = mt->getFirstRegisterToPass(&it);
+          reg != R_ZERO;
+          reg = mt->getNextRegisterToPass(&it))
         {
-          int idx = mt->getFunctionIndexByAddress(fn->getAddress());
+          if (reg == R_SP)
+            { emit->generic("sp"); comma = ","; }
+          if (reg == R_FNA)
+            {
+              int idx = mt->getFunctionIndexByAddress(fn->getAddress());
 
-          panic_if(idx < 0, "Could not find function index for function %s in method %s",
-              fn->getName(), mt->getName());
-          emit->generic("%s %d", comma, idx);
+              panic_if(idx < 0, "Could not find function index for function %s in method %s",
+                  fn->getName(), mt->getName());
+              emit->generic("%s %d", comma, idx);
+            }
+          if (reg == R_A0)
+            { emit->generic("%s a0", comma); comma = ","; }
+          if (reg == R_A1)
+            { emit->generic("%s a1", comma); comma = ","; }
+          if (reg == R_A2)
+            { emit->generic("%s a2", comma); comma = ","; }
+          if (reg == R_A3)
+            { emit->generic("%s a3", comma); comma = ","; }
         }
       emit->generic("); break;\n");
     }

tools/compiled/translator/include/javamethod.hh
@@ -72,6 +72,8 @@ public:
 
   MIPS_register_t getNextRegisterToPass(void *it);
 
+  bool registerIsArgument(MIPS_register_t reg);
+
   /**
    * The size of the return value, in words.
    *
@@ -163,6 +165,9 @@ protected:
   size_t maxStackHeight;
 
   int m_returnSize;
+
+  /* M_ZERO-terminated list */
+  MIPS_register_t *m_possibleArguments;
 };
 
 class CallTableMethod : public JavaMethod

commit 2cb2b97d30128f2829ccc85526e301ff63141db5

translator: Correct handling of unknown number of arguments
Second part of correcting regression introduced in

   167ea80eff96fee40475e6cdc780937d0bfb8c5f

The problem was that unknown number of arguments (tagged by -1) would
be parsed as 0 arguments!

tools/compiled/translator/javamethod.cc
@@ -175,7 +175,7 @@ bool JavaMethod::pass1()
 
   ElfSymbol *sym = elf->getSymbolByAddr(this->getAddress());
   /* First see which arguments need to be passed */
-  if (config->optimizeFunctionReturnArguments && sym)
+  if (config->optimizeFunctionReturnArguments && sym && sym->n_args >= 0)
     {
       /* Zero-terminate at the last needed reg */
       if (sym->n_args <= 3)