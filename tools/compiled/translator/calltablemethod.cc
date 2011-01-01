SimonKagstrom/cibyl

commit f00b820e839f031953e4a98da16617b148bd9db8

translator: Implement lookup of return value/argument size via DWARF
This allows return values (this commit) and arguments (a later commit)
to be squeezed into the actual number of arguments passed in the
original function.

Access this via the --optimize-function-arguments option.

tools/compiled/translator/calltablemethod.cc
@@ -103,13 +103,13 @@ void CallTableMethod::generateMethod(const char *name,
       emit->generic("      case 0x%x:  ", fn->getAddress());
       if (config->threadSafe)
         {
-          if (mt->clobbersReg( R_V0 ) && mt->clobbersReg( R_V1) )
+          if (mt->returnSize() == 2)
             emit->generic("ret = ");
-          else if (mt->clobbersReg( R_V0 ) || mt->clobbersReg( R_V1) ) /* Only one */
+          else if (mt->returnSize() == 1) /* Only one */
             emit->generic("ret = (int)");
           /* else nothing */
         }
-      else if (mt->clobbersReg( R_V0 ))
+      else if (mt->returnSize() == 1)
         emit->generic("ret = ");
       emit->generic("%s.%s(", cl->getName(), mt->getName());
 

tools/cibyl-mips2java
@@ -162,6 +162,9 @@ a function. The number of arguments passed to leaf functions can also
 be reduced. This reduces the size of the executable and can provide
 performance advantages for frequently called short functions""",
 		  dest="optimize_register_scheduling")
+parser.add_option("--optimize-function-arguments", action="store_true", default=False,
+				  help="""Lookup the number of arguments and return values from the DWARF information in the input program to determine register usage. Typically reduces the size by quite a bit, especially with small functions.""",
+		  dest="optimize_function_arguments")
 
 (options, args) = parser.parse_args()
 if len(args) != 1:
@@ -176,6 +179,7 @@ config.doOptimizeIndirectCalls = options.doOptimizeIndirectCalls
 config.doPeepholeOptimize = options.peepholeOptimize
 config.doRegisterValueTracking = options.registerValueTracking
 config.doOptimizePruneStackStores = options.doOptimizePruneStackStores
+config.doOptimizeFunctionArguments = options.optimize_function_arguments
 
 config.pruneUnusedFunctions = options.pruneUnusedFunctions
 config.threadSafe = options.threadSafe

tools/compiled/translator/Makefile
@@ -10,8 +10,9 @@
 ##
 ######################################################################
 HOST_CXX ?= g++
+HOST_CC ?= gcc
 HOST_LD ?= g++
-HOST_CFLAGS ?=-ggdb -Iinclude -Wall
+HOST_CFLAGS ?=-ggdb -Iinclude -Ilib/dwarves/ -Ilib/dwarves/include/ -Wall
 
 ERROR_FILTER := 2>&1 | sed -e 's/\(.[a-zA-Z]\+\):\([0-9]\+\):/\1(\2):/g'
 
@@ -20,15 +21,19 @@ SRCS=instruction.cc basicblock.cc controller.cc javamethod.cc \
      registerallocator.cc calltablemethod.cc codeblock.cc \
      string-instruction.cc builtins.cc syscall-wrappers.cc \
      functioncolocation.cc
+     
+SRCS_C=mips-dwarf.c
 
 OBJS=$(patsubst %.cc,objs/%.oo,$(SRCS))
+OBJS_C=$(patsubst %.c,objs/%.o,$(SRCS_C))
 DEPS=$(patsubst %.cc,deps/%.d,$(SRCS))
+DEPS_c=$(patsubst %.c,deps/%.d,$(SRCS_C))
 
 #GCOV=-fprofile-arcs -ftest-coverage
 
 all: $(DEPS) xcibyl-translator
 
--include $(DEPS)
+-include $(DEPS) $(DEPS_C)
 
 clean:
 	rm -rf objs/* deps/* *.gcda *.gcno *~ xcibyl-translator xcibyl-translator-gcov lib/libghthash-0.6.2 lib/.libghthash
@@ -37,14 +42,25 @@ objs/%.oo: %.cc
 	@echo CXX $(notdir $<)
 	@$(HOST_CXX) $(GCOV) -Ilib/libghthash-0.6.2/src/ $(HOST_CFLAGS) -c -o $@ $< $(ERROR_FILTER)
 
+objs/%.o: %.c
+	@echo CXX $(notdir $<)
+	@$(HOST_CC) $(GCOV) -Ilib/libghthash-0.6.2/src/ $(HOST_CFLAGS) -c -o $@ $< $(ERROR_FILTER)
+
 
 deps/%.d: %.cc
 	@$(HOST_CXX) -MM -MT '$(patsubst %.cc,objs/%.oo,$<)' $(GCOV) -Ilib/libghthash-0.6.2/src/ $(HOST_CFLAGS) -o $@ $<
+
+deps/%.d: %.c
+	@$(HOST_CC) -MM -MT '$(patsubst %.cc,objs/%.oo,$<)' $(GCOV) -Ilib/libghthash-0.6.2/src/ $(HOST_CFLAGS) -o $@ $<
 
 lib/.libghthash:
 	cd lib && tar -xzf libghthash-0.6.2.tar.gz
 	cd lib/libghthash-0.6.2/ && ./configure && make
 	touch $@
 
-xcibyl-translator: lib/.libghthash $(OBJS)
-	$(HOST_LD) $(GCOV) -Llib/libghthash-0.6.2/src/.libs/ -o $@ $(OBJS) -lghthash -ldwarf -lelf -static
+lib/.dwarves:
+	cd lib/dwarves/ && make
+	touch $@
+
+xcibyl-translator: lib/.libghthash $(OBJS) $(OBJS_C)
+	$(HOST_LD) $(GCOV) -Llib/libghthash-0.6.2/src/.libs/ -o $@ $(OBJS) $(OBJS_C) -lghthash -ldw -lelf -static

tools/compiled/translator/elf.cc
@@ -16,6 +16,9 @@
 #include <stdio.h>
 #include <assert.h>
 #include <fcntl.h>
+#include <libelf.h>
+#include <dwarf.h>
+#include <elfutils/libdw.h>
 
 #include <utils.h>
 #include <elf.hh>
@@ -56,9 +59,14 @@ void CibylElf::handleSymtab(Elf_Scn *scn)
       int binding = ELF32_ST_BIND(s->st_info);
 
       /* Ohh... This is an interesting symbol, add it! */
-      if ( type == STT_FUNC)
-        this->symbols[n_syms++] = this->functionSymbols[n_fns++] = new ElfSymbol(i, binding, s->st_value,
-                                                                                 s->st_size, type, sym_name);
+      if ( type == STT_FUNC) {
+          ElfSymbol *sym = new ElfSymbol(i, binding, s->st_value,
+              s->st_size, type, sym_name);
+
+          this->symbols[n_syms++] = this->functionSymbols[n_fns++] = sym;
+          ght_insert(this->symbols_by_addr, (void*)sym,
+              sizeof(sym->addr), (void*)&sym->addr);
+      }
       else if (type == STT_OBJECT || type == STT_COMMON || type == STT_TLS)
         this->symbols[n_syms++] = this->dataSymbols[n_datas++] = new ElfSymbol(i, binding, (uint32_t)s->st_value,
                                                                                (uint32_t)s->st_size,
@@ -95,6 +103,61 @@ static int reloc_cmp(const void *_a, const void *_b)
   return addr_diff;
 }
 
+extern "C" int mips_arg_size (Elf *elf, Dwarf_Die *functypedie, Dwarf_Attribute *attr);
+
+void CibylElf::handleDwarfFunction(Dwarf_Die *fun_die)
+{
+  Dwarf_Die result;
+  Dwarf_Attribute attr_mem;
+  Dwarf_Attribute *attr;
+  Dwarf_Addr addr;
+
+  if (dwarf_lowpc(fun_die, &addr) != 0)
+    return;
+  ElfSymbol *sym = this->getSymbolByAddr(addr);
+
+  if (!sym)
+    return;
+
+  if (dwarf_child (fun_die, &result) != 0)
+    return;
+
+  attr = dwarf_attr_integrate(fun_die, DW_AT_type, &attr_mem);
+
+  sym->ret_size = mips_arg_size(elf, fun_die, attr);
+
+  int n_args = 0;
+  do {
+      int arg_size;
+
+      switch (dwarf_tag (&result))
+      {
+      case DW_TAG_formal_parameter:
+        attr = dwarf_attr_integrate(&result, DW_AT_type, &attr_mem);
+        arg_size = mips_arg_size(elf, fun_die, attr);
+
+        if (arg_size != 1)
+          {
+            /* Better safe than sorry - skip this */
+            n_args = -1;
+            break;
+          }
+
+        n_args++;
+        break;
+      case DW_TAG_inlined_subroutine:
+        /* Recurse further down */
+        this->handleDwarfFunction(&result);
+        break;
+      default:
+        break;
+      }
+  } while(dwarf_siblingof(&result, &result) == 0);
+
+  sym->n_args = n_args;
+}
+
+
 CibylElf::CibylElf(const char *filename)
 {
   Elf_Scn *scn = NULL;
@@ -103,6 +166,7 @@ CibylElf::CibylElf(const char *filename)
   int fd;
 
   this->symtable = ght_create(1024);
+  this->symbols_by_addr = ght_create(1024);
   this->sections_by_name = ght_create(32);
   this->relocations_by_symbol = ght_create(512);
 
@@ -220,8 +284,44 @@ CibylElf::CibylElf(const char *filename)
     this->fixupSymbolSize(this->dataSymbols,
                           this->n_dataSymbols,
                           this->getSection(".data")->size);
-
   close(fd);
+
+
+  Dwarf_Off last_offset = 0;
+  Dwarf_Off offset = 0;
+  size_t hdr_size;
+  Dwarf *dbg;
+
+  /* Initialize libdwarf */
+  dbg = dwarf_begin_elf(this->elf, DWARF_C_READ, NULL);
+  if (!dbg)
+      panic("No DWARF info???");
+
+  while (dwarf_nextcu(dbg, offset, &offset, &hdr_size, 0, 0, 0) == 0) {
+      Dwarf_Die result, cu_die;
+
+      if (dwarf_offdie(dbg, last_offset + hdr_size, &cu_die) == NULL)
+        continue;
+      last_offset = offset;
+
+      if (dwarf_child (&cu_die, &result) != 0)
+        continue;
+
+      do {
+          switch (dwarf_tag(&result))
+          {
+          case DW_TAG_subprogram:
+          case DW_TAG_entry_point:
+          case DW_TAG_inlined_subroutine:
+            this->handleDwarfFunction(&result);
+            break;
+          default:
+            break;
+          }
+      } while(dwarf_siblingof(&result, &result) == 0);
+  }
+
+  dwarf_end(dbg);
 }
 
 void CibylElf::fixupSymbolSize(ElfSymbol **table, int n, uint32_t sectionEnd)
@@ -276,4 +376,11 @@ ElfReloc *CibylElf::getRelocationBySymbol(ElfSymbol *sym)
 {
   return (ElfReloc*)ght_get(this->relocations_by_symbol,
                             sizeof(ElfSymbol*), sym);
+}
+
+ElfSymbol *CibylElf::getSymbolByAddr(unsigned long addr)
+{
+  return (ElfSymbol *)ght_get(this->symbols_by_addr,
+      sizeof(uint32_t), (void*)&addr);
+
 }

tools/compiled/translator/include/elf.hh
@@ -16,6 +16,7 @@
 #include <stdlib.h>
 
 #include <libelf.h>
+#include <elfutils/libdw.h>
 #include <ght_hash_table.h>
 
 class CibylElf;
@@ -31,6 +32,10 @@ public:
     this->size = size;
     this->type = type;
     this->name = name;
+
+    /* Unknown */
+    this->ret_size = -1;
+    this->n_args = -1;
   }
 
   int index;
@@ -39,6 +44,9 @@ public:
   uint32_t size;
   int type;
   const char *name;
+
+  int ret_size;
+  int n_args;
 };
 
 
@@ -95,6 +103,8 @@ public:
 
   ElfSymbol **getFunctions();
 
+  ElfSymbol *getSymbolByAddr(unsigned long addr);
+
   int getNumberOfFunctions();
 
   ElfReloc **getRelocations();
@@ -125,6 +135,8 @@ private:
 
   void fixupSymbolSize(ElfSymbol **table, int n, uint32_t sectionEnd);
 
+  void handleDwarfFunction(Dwarf_Die *fun_die);
+
   static CibylElf *instance;
 
   Elf *elf; /* from libelf */
@@ -137,6 +149,7 @@ private:
   int n_functionSymbols;
   int n_dataSymbols;
   ght_hash_table_t *symtable;
+  ght_hash_table_t *symbols_by_addr;
 
   ght_hash_table_t *sections_by_name;
   ght_hash_table_t *relocations_by_symbol;

tools/compiled/translator/javamethod.cc
@@ -116,16 +116,16 @@ char *JavaMethod::getJavaMethodName()
 
   if ( config->threadSafe )
     {
-      if ( this->clobbersReg(R_V0) && this->clobbersReg(R_V1) )
+      if ( this->returnSize() == 2)
         this->javaName[o++] = 'J'; /* long */
-      else if ( this->clobbersReg(R_V0) || this->clobbersReg(R_V1) )
+      else if ( this->returnSize() == 1)
         this->javaName[o++] = 'I';
       else
         this->javaName[o++] = 'V';
     }
   else
     {
-      if ( this->clobbersReg(R_V0) )
+      if ( this->returnSize() >= 1)
         this->javaName[o++] = 'I';
       else
         this->javaName[o++] = 'V';
@@ -179,6 +179,14 @@ bool JavaMethod::pass1()
     this->setReturnSize(1);
   else if (this->clobbersReg(R_V1))
     emit->warning("Function clobbers V1 but not V0");
+
+  /* Update the size from the ELF, if it's known */
+  ElfSymbol *sym = elf->getSymbolByAddr(this->getAddress());
+  if (config->optimizeFunctionReturnArguments && sym)
+    {
+      if (sym->ret_size >= 0 && sym->ret_size <= 2)
+        this->setReturnSize(sym->ret_size);
+    }
 
   return out;
 }
@@ -443,7 +451,7 @@ bool JavaMethod::pass2()
 
   if (config->threadSafe)
     {
-      if (this->clobbersReg(R_V1) && this->clobbersReg(R_V0))
+      if (this->returnSize() == 2)
         {
           /* Return a 64-bit value */
           emit->bc_pushregister(R_V1);
@@ -455,12 +463,7 @@ bool JavaMethod::pass2()
           emit->bc_lor(); /* push (v1 << 32) | v0 */
           emit->bc_lreturn();
         }
-      else if (this->clobbersReg(R_V1)) /* Only v1 */
-        {
-          emit->bc_pushregister(R_V1);
-          emit->bc_ireturn();
-        }
-      else if (this->clobbersReg(R_V0)) /* Only v0 */
+      else if (this->returnSize() == 1) /* Only v0 */
         {
           emit->bc_pushregister(R_V0);
           emit->bc_ireturn();
@@ -471,13 +474,13 @@ bool JavaMethod::pass2()
   else
     {
       /* Not thread safe */
-      if (this->clobbersReg(R_V1))
+      if (this->returnSize() == 2)
         {
           emit->bc_pushregister(R_V1);
           emit->bc_putstatic("%sCRunTime/saved_v1 I",
               controller->getJasminPackagePath());
         }
-      if (this->clobbersReg(R_V0))
+      if (this->returnSize() >= 1)
         {
           emit->bc_pushregister(R_V0);
           emit->bc_ireturn();
@@ -517,7 +520,7 @@ MIPS_register_t JavaMethod::getNextRegisterToPass(void *_it)
 
 int JavaMethod::returnSize()
 {
-  panic_if(this->m_returnSize < 0,
+  panic_if(this->m_returnSize < 0 || this->m_returnSize > 2,
       "returnSize accessed too soon for %s %d\n",
       this->getJavaMethodName(), this->m_returnSize);
   return this->m_returnSize;

tools/compiled/translator/mips-dwarf.c
@@ -1,0 +1,281 @@
+/* All this is from elfutils */
+#include <string.h>
+#include <dwarf.h>
+#include <elfutils/libdw.h>
+
+/* The ABI of the file.  Also see EF_MIPS_ABI2 above. */
+#define EF_MIPS_ABI             0x0000F000
+
+/* The original o32 abi. */
+#define E_MIPS_ABI_O32          0x00001000
+
+/* O32 extended to work on 64 bit architectures */
+#define E_MIPS_ABI_O64          0x00002000
+
+/* EABI in 32 bit mode */
+#define E_MIPS_ABI_EABI32       0x00003000
+
+/* EABI in 64 bit mode */
+#define E_MIPS_ABI_EABI64       0x00004000
+
+/* All the possible MIPS ABIs. */
+enum mips_abi
+  {
+    MIPS_ABI_UNKNOWN = 0,
+    MIPS_ABI_N32,
+    MIPS_ABI_O32,
+    MIPS_ABI_N64,
+    MIPS_ABI_O64,
+    MIPS_ABI_EABI32,
+    MIPS_ABI_EABI64,
+    MIPS_ABI_LAST
+  };
+
+/* Find the mips ABI of the current file */
+enum mips_abi find_mips_abi(Elf *elf)
+{
+  GElf_Ehdr ehdr_mem;
+  GElf_Ehdr *ehdr = gelf_getehdr (elf, &ehdr_mem);
+
+  if (ehdr == NULL)
+    return MIPS_ABI_LAST;
+
+  GElf_Word elf_flags = ehdr->e_flags;
+
+  /* Check elf_flags to see if it specifies the ABI being used.  */
+  switch ((elf_flags & EF_MIPS_ABI))
+    {
+    case E_MIPS_ABI_O32:
+      return MIPS_ABI_O32;
+    case E_MIPS_ABI_O64:
+      return MIPS_ABI_O64;
+    case E_MIPS_ABI_EABI32:
+      return MIPS_ABI_EABI32;
+    case E_MIPS_ABI_EABI64:
+      return MIPS_ABI_EABI64;
+    default:
+      if ((elf_flags & EF_MIPS_ABI2))
+        return MIPS_ABI_N32;
+    }
+
+  /* GCC creates a pseudo-section whose name describes the ABI.  */
+  size_t shstrndx;
+  if (elf_getshdrstrndx (elf, &shstrndx) < 0)
+    return MIPS_ABI_LAST;
+
+  const char *name;
+  Elf_Scn *scn = NULL;
+  while ((scn = elf_nextscn (elf, scn)) != NULL)
+    {
+      GElf_Shdr shdr_mem;
+      GElf_Shdr *shdr = gelf_getshdr (scn, &shdr_mem);
+      if (shdr == NULL)
+        return MIPS_ABI_LAST;
+
+      name = elf_strptr (elf, shstrndx, shdr->sh_name) ?: "";
+      if (strncmp (name, ".mdebug.", 8) != 0)
+        continue;
+
+      if (strcmp (name, ".mdebug.abi32") == 0)
+        return MIPS_ABI_O32;
+      else if (strcmp (name, ".mdebug.abiN32") == 0)
+        return MIPS_ABI_N32;
+      else if (strcmp (name, ".mdebug.abi64") == 0)
+        return MIPS_ABI_N64;
+      else if (strcmp (name, ".mdebug.abiO64") == 0)
+        return MIPS_ABI_O64;
+      else if (strcmp (name, ".mdebug.eabi32") == 0)
+        return MIPS_ABI_EABI32;
+      else if (strcmp (name, ".mdebug.eabi64") == 0)
+        return MIPS_ABI_EABI64;
+      else
+        return MIPS_ABI_UNKNOWN;
+    }
+
+  return MIPS_ABI_UNKNOWN;
+}
+
+unsigned int
+mips_abi_regsize (enum mips_abi abi)
+{
+  switch (abi)
+    {
+    case MIPS_ABI_EABI32:
+    case MIPS_ABI_O32:
+      return 4;
+    case MIPS_ABI_N32:
+    case MIPS_ABI_N64:
+    case MIPS_ABI_O64:
+    case MIPS_ABI_EABI64:
+      return 8;
+    case MIPS_ABI_UNKNOWN:
+    case MIPS_ABI_LAST:
+    default:
+      return 0;
+    }
+}
+
+
+/* $v0 or pair $v0, $v1 */
+static const Dwarf_Op loc_intreg_o32[] =
+  {
+    { .atom = DW_OP_reg2 }, { .atom = DW_OP_piece, .number = 4 },
+    { .atom = DW_OP_reg3 }, { .atom = DW_OP_piece, .number = 4 },
+  };
+
+static const Dwarf_Op loc_intreg[] =
+  {
+    { .atom = DW_OP_reg2 }, { .atom = DW_OP_piece, .number = 8 },
+    { .atom = DW_OP_reg3 }, { .atom = DW_OP_piece, .number = 8 },
+  };
+#define nloc_intreg     1
+#define nloc_intregpair 4
+
+/* $f0 (float), or pair $f0, $f1 (double).
+ * f2/f3 are used for COMPLEX (= 2 doubles) returns in Fortran */
+static const Dwarf_Op loc_fpreg_o32[] =
+  {
+    { .atom = DW_OP_regx, .number = 32 }, { .atom = DW_OP_piece, .number = 4 },
+    { .atom = DW_OP_regx, .number = 33 }, { .atom = DW_OP_piece, .number = 4 },
+    { .atom = DW_OP_regx, .number = 34 }, { .atom = DW_OP_piece, .number = 4 },
+    { .atom = DW_OP_regx, .number = 35 }, { .atom = DW_OP_piece, .number = 4 },
+  };
+
+/* $f0, or pair $f0, $f2.  */
+static const Dwarf_Op loc_fpreg[] =
+  {
+    { .atom = DW_OP_regx, .number = 32 }, { .atom = DW_OP_piece, .number = 8 },
+    { .atom = DW_OP_regx, .number = 34 }, { .atom = DW_OP_piece, .number = 8 },
+  };
+#define nloc_fpreg  1
+#define nloc_fpregpair 4
+#define nloc_fpregquad 8
+
+/* The return value is a structure and is actually stored in stack space
+   passed in a hidden argument by the caller.  But, the compiler
+   helpfully returns the address of that space in $v0.  */
+static const Dwarf_Op loc_aggregate[] =
+  {
+    { .atom = DW_OP_breg2, .number = 0 }
+  };
+#define nloc_aggregate 1
+
+int mips_arg_size (Elf *elf, Dwarf_Die *functypedie, Dwarf_Attribute *attr)
+{
+  Dwarf_Attribute attr_mem;
+
+  /* First find the ABI used by the elf object */
+  enum mips_abi abi = find_mips_abi(elf);
+
+  /* Something went seriously wrong while trying to figure out the ABI */
+  if (abi == MIPS_ABI_LAST)
+    return -1;
+
+  /* We couldn't identify the ABI, but the file seems valid */
+  if (abi == MIPS_ABI_UNKNOWN)
+    return -2;
+
+  /* Can't handle EABI variants */
+  if ((abi == MIPS_ABI_EABI32) || (abi == MIPS_ABI_EABI64))
+    return -2;
+
+  unsigned int regsize = mips_abi_regsize (abi);
+  if (!regsize)
+    return -2;
+
+  /* Start with the function's type, and get the DW_AT_type attribute,
+     which is the type of the return value.  */
+  if (attr == NULL)
+    /* The function has no return value, like a `void' function in C.  */
+    return 0;
+
+  Dwarf_Die die_mem;
+  Dwarf_Die *typedie = dwarf_formref_die (attr, &die_mem);
+  int tag = dwarf_tag (typedie);
+
+  /* Follow typedefs and qualifiers to get to the actual type.  */
+  while (tag == DW_TAG_typedef
+         || tag == DW_TAG_const_type || tag == DW_TAG_volatile_type
+         || tag == DW_TAG_restrict_type || tag == DW_TAG_mutable_type)
+    {
+      attr = dwarf_attr_integrate (typedie, DW_AT_type, &attr_mem);
+      typedie = dwarf_formref_die (attr, &die_mem);
+      tag = dwarf_tag (typedie);
+    }
+
+  switch (tag)
+    {
+    case -1:
+      return -1;
+
+    case DW_TAG_subrange_type:
+      if (! dwarf_hasattr_integrate (typedie, DW_AT_byte_size))
+        {
+          attr = dwarf_attr_integrate (typedie, DW_AT_type, &attr_mem);
+          typedie = dwarf_formref_die (attr, &die_mem);
+          tag = dwarf_tag (typedie);
+        }
+      /* Fall through.  */
+
+    case DW_TAG_base_type:
+    case DW_TAG_enumeration_type:
+    case DW_TAG_pointer_type:
+    case DW_TAG_ptr_to_member_type:
+      {
+        Dwarf_Word size;
+        if (dwarf_formudata (dwarf_attr_integrate (typedie, DW_AT_byte_size,
+                                         &attr_mem), &size) != 0)
+          {
+            if (tag == DW_TAG_pointer_type || tag == DW_TAG_ptr_to_member_type)
+              size = regsize;
+            else
+              return -1;
+          }
+        if (tag == DW_TAG_base_type)
+          {
+            Dwarf_Word encoding;
+            if (dwarf_formudata (dwarf_attr_integrate (typedie, DW_AT_encoding,
+                                             &attr_mem), &encoding) != 0)
+              return -1;
+
+#define ABI_LOC(loc, regsize) ((regsize) == 4 ? (loc ## _o32) : (loc))
+
+            if (encoding == DW_ATE_float)
+              {
+                if (size <= regsize)
+                    return nloc_fpreg;
+
+                if (size <= 2*regsize)
+                  return nloc_fpregpair;
+
+                if (size <= 4*regsize && abi == MIPS_ABI_O32)
+                  return nloc_fpregquad;
+
+                goto aggregate;
+              }
+          }
+        if (size <= regsize)
+          return nloc_intreg;
+        if (size <= 2*regsize)
+          return nloc_intregpair;
+
+        /* Else fall through. Shouldn't happen though (at least with gcc) */
+      }
+
+    case DW_TAG_structure_type:
+    case DW_TAG_class_type:
+    case DW_TAG_union_type:
+    case DW_TAG_array_type:
+    aggregate:
+      /* XXX TODO: Can't handle structure return with other ABI's yet :-/ */
+      if ((abi != MIPS_ABI_O32) && (abi != MIPS_ABI_O64))
+        return -2;
+
+      return nloc_aggregate;
+    }
+
+  /* XXX We don't have a good way to return specific errors from ebl calls.
+     This value means we do not understand the type, but it is well-formed
+     DWARF and might be valid.  */
+  return -2;
+}

tools/python/Cibyl/CompiledHelpers/helpers.py
@@ -44,6 +44,8 @@ def doTranslation(filename, syscallDirectories):
         conf = conf + "prune_call_table=1,"
     if config.doOptimizePruneStackStores:
         conf = conf + "optimize_prune_stack_stores=1,"
+    if config.doOptimizeFunctionArguments:
+        conf = conf + "optimize_function_return_arguments=1,"
     if config.threadSafe:
         conf = conf + "thread_safe=1,"
     if len(config.colocateFunctions) > 0:

commit f4ee8a0b165ef875995664e5f172e89fe77fc5f8

translator: Correct return value handling for the calltable method
Fixes a regression introduced in commit

   f00b820e839f031953e4a98da16617b148bd9db8

The calltable method should pickup the return value (v0) if 1 *or* 2
return values are produced. The current implementation only does this
for one return value, which breaks things badly.

tools/compiled/translator/calltablemethod.cc
@@ -109,7 +109,7 @@ void CallTableMethod::generateMethod(const char *name,
             emit->generic("ret = (int)");
           /* else nothing */
         }
-      else if (mt->returnSize() == 1)
+      else if (mt->returnSize() >= 1)
         emit->generic("ret = ");
       emit->generic("%s.%s(", cl->getName(), mt->getName());
 