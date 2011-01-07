d33ff763a0f180f5e3e230df080ae421e317150e
Simon Kagstrom <simon.kagstrom@gmail.com>
2011-01-07 12:11:28 +0100

    libs: Remove weak-main.c again (it's defined in crt0.S)

--- libs/src/libgcc/weak-main.c
+++
@@ -1,9 +0,0 @@
-/*
- * Define a weak main symbol which will be overridden by the real main.
- *
- * The purpose of this is for crt0.o to have something to call for
- * pure-Java-libraries.
- */
-void __attribute__((weak)) main(void)
-{
-}
