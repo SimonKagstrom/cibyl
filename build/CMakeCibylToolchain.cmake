5041bb19991f47b1e4d9fffcbce27cb164c7dd5c
Simon Kagstrom <simon.kagstrom@gmail.com>
2011-01-15 10:01:00 +0100

    build: Lookup the Sun Wireless Toolkit via the preverify path

--- build/CMakeCibylToolchain.cmake
+++ build/CMakeCibylToolchain.cmake
@@ -15,3 +15,24 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 set(CMAKE_ASM_FLAGS "-c")
+
+
+
+
+execute_process(COMMAND which preverify
+  OUTPUT_VARIABLE WTK_PREVERIFY_PATH
+  RESULT_VARIABLE which_RES)
+
+if (which_RES)
+  message("*** WARNING: Can't find Sun preverify command (for J2ME)\n"
+"*** Add it to your PATH, or Cibyl won't be able to build J2ME programs.")
+  set (WTK_PATH /)
+else (which_RES)
+  execute_process(COMMAND readlink -f ${WTK_PREVERIFY_PATH}
+    OUTPUT_VARIABLE WTK_PREVERIFY_READLINK)
+  execute_process(COMMAND dirname ${WTK_PREVERIFY_READLINK}
+    OUTPUT_VARIABLE WTK_PREVERIFY_PATH)
+  string(STRIP ${WTK_PREVERIFY_PATH} WTK_PREVERIFY_PATH)
+  set (WTK_PATH ${WTK_PREVERIFY_PATH}/../)
+endif (which_RES)
+
