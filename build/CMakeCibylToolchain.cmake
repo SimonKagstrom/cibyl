75c4ae7c0390f241c324bab0dd0fb08799a0f0dc
Simon Kagstrom <simon.kagstrom@gmail.com>
2011-01-15 08:25:09 +0100

    build: Set assembly compiler as well
    Set to GCC since we want to have it pre-processed

--- build/CMakeCibylToolchain.cmake
+++ build/CMakeCibylToolchain.cmake
@@ -4,6 +4,7 @@ set(CMAKE_SYSTEM_NAME Cibyl)
 set(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
+set(CMAKE_ASM_COMPILER  mips-cibyl-elf-gcc)
 set(CMAKE_C_COMPILER   mips-cibyl-elf-gcc)
 set(CMAKE_CXX_COMPILER mips-cibyl-elf-g++)
 
@@ -12,3 +13,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_ASM_FLAGS "-c")
